4d2e06d4b9ebf81d28262a3e2a83e5c5ebec7916
Gyan Ratan <gyanratanazad12345@gmail.com>
2025-02-22 13:48:14 +0530

    product_services added

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -1,10 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class AuthService{
-
+class AuthService {
   Future<String> createAccountWithEmail(String email, String password) async {
     try {
-      UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      UserCredential userCredential =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
         email: email,
         password: password,
       );
@@ -16,26 +17,39 @@ class AuthService{
     }
   }
 
-  Future<String> loginWithEmailAndPassword(String email, String password) async{
-    try{
-      await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
+  Future<String> loginWithEmailAndPassword(
+      String email, String password) async {
+    try {
+      await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
       return "Login successful";
-    } on FirebaseAuthException catch(e){
+    } on FirebaseAuthException catch (e) {
       return e.message.toString();
     }
   }
 
-  Future<String> logOut() async{
-    try{
+  Future<String> logOut() async {
+    try {
       await FirebaseAuth.instance.signOut();
       return "Logout Successful";
-    } on FirebaseAuthException catch(e){
+    } on FirebaseAuthException catch (e) {
       return e.message.toString();
     }
   }
 
-  Future<bool> isLoggedIn() async{
+  Future<bool> isLoggedIn() async {
     var user = FirebaseAuth.instance.currentUser;
     return user != null;
+  }
+
+  // add name in users collection
+  Future<String> addName(String name, String uid) async {
+    CollectionReference users = FirebaseFirestore.instance.collection('users');
+    try {
+      await users.add({'name': name, 'uid': uid});
+      return "Name Updated.";
+    } catch (error) {
+      return "Error in updating name";
+    }
   }
 }
---
+++ lib/services/product_services.dart
@@ -0,0 +1,56 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ProductServices {
+  // add product in product collection
+  Future<String> addProduct(
+      String product,
+      String brand,
+      String mrp,
+      String uid,
+      List<String> categories,
+      String thumbnail,
+      String image,
+      String description,
+      bool isSold) async {
+    CollectionReference products =
+        FirebaseFirestore.instance.collection('products');
+    try {
+      await products.add({
+        'product': product,
+        'brand': brand,
+        'price': mrp,
+        'uid': uid,
+        'categories': categories,
+        'thumbnail': thumbnail,
+        'image': image,
+        'description': description,
+        'isSold': false
+      });
+      return "product added successfully";
+    } catch (error) {
+      return "Product can't be added";
+    }
+  }
+
+  // search product in product collection
+  Future<List<Map<String, dynamic>>> searchProduct(
+      String query, double minPrice, double maxPrice) async {
+    try {
+      CollectionReference products =
+          FirebaseFirestore.instance.collection('products');
+      QuerySnapshot querySnapshot = await products
+          .where('product', isGreaterThanOrEqualTo: query)
+          .where('product', isLessThanOrEqualTo: "$query\uf8ff")
+          .where('price', isGreaterThanOrEqualTo: minPrice)
+          .where('price', isLessThanOrEqualTo: maxPrice)
+          .get();
+      List<Map<String, dynamic>> productList = querySnapshot.docs
+          .map((doc) => doc.data() as Map<String, dynamic>)
+          .toList();
+      return productList;
+    } catch (error) {
+      print("Error searching product: $error");
+      return [];
+    }
+  }
+}
