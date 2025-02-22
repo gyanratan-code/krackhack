263127f549e64c2e79367300d16a70936d61c8f5
Gyan Ratan <gyanratanazad12345@gmail.com>
2025-02-22 16:20:30 +0530

    cloudinary services added

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:iit_marketing/views/addItem.dart';
 import 'package:iit_marketing/views/home.dart';
